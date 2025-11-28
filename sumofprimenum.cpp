936671ad826e6c112dc67c7d928b833acd00c618
Monali Nagardhankar <nmonali12@gmail.com>
2025-11-28 22:08:03 +0530

    Add files via upload

---
+++ sumofprimenum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int start, end;
+    cout << "Enter start of range: ";
+    cin >> start;
+    cout << "Enter end of range: ";
+    cin >> end;
+
+    int sum = 0;
+
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            sum += i;
+        }
+    }
+
+    cout << "Sum of prime numbers between "
+         << start << " and " << end << " = " << sum << endl;
+
+    return 0;
+}
