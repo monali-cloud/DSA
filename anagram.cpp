348ebc9421e7101568f831b7d88fa422a6775ce9
Monali Nagardhankar <nmonali12@gmail.com>
2025-11-30 16:05:45 +0530

    Add files via upload

---
+++ anagram.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cout << "Enter first string: ";
+    cin >> s1;
+    cout << "Enter second string: ";
+    cin >> s2;
+
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+
+    if (s1 == s2)
+        cout << "Strings are anagrams";
+    else
+        cout << "Strings are NOT anagrams";
+
+    return 0;
+}
