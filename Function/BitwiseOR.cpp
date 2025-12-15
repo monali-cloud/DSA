7d0c5ba8e7a1c2b89ee8091df47bc9eb83365703
Monali Nagardhankar <nmonali12@gmail.com>
2025-12-15 19:43:07 +0530

    Add files via upload

---
+++ Function/BitwiseOR.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=6,b=10;   // 6=0110 ; 10= 1010
+    cout<<(a | b)<<endl;   //ans = 1110  i.e 14
+
+    return 0;
+}
---
+++ Function/BitwiseXOR.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int a=6, b=10;   // 6= 0110 , 10 = 1010 
+    cout<<(a ^ b)<<endl;  //ans = 1100
+
+    return 0;
+}
